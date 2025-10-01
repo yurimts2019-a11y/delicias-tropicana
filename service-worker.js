3ab755ebf106ff74edd2201d04611248679b44db
yurimts2019-a11y <yurimts2019@gmail.com>
2025-10-01 11:47:20 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,21 @@
-// Service Worker
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('tropicana-cache').then(cache => {
-      return cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png']);
+    caches.open("tropicana-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
